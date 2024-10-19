156f3c534e0b84714a96b826e661d1927bbfd698
VladMakhun <147133088+VladMakhun@users.noreply.github.com>
2024-10-19 15:26:29 +0300

    Update HackerRank.rs

--- src/HackerRank.rs
+++ src/HackerRank.rs
@@ -1,1 +1,259 @@
+//Simple Array Sum 1
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+fn simpleArraySum(ar: &[i32]) -> i32 {
+    ar.iter().sum()
+
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let _ar_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let ar: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = simpleArraySum(&ar);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
+
+
+//Compare the Triplets 2
+
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+fn compareTriplets(a: &[i32], b: &[i32]) -> Vec<i32> {
+    let mut score_a = 0;
+    let mut score_b = 0;
+
+    for (val_a, val_b) in a.iter().zip(b.iter()) {
+        if val_a > val_b {
+            score_a += 1;
+        } else if val_a < val_b {
+            score_b += 1;
+        }
+    }
+
+    vec![score_a, score_b]
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let a: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let b: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = compareTriplets(&a, &b);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            write!(&mut fptr, " ").ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
+
+
+// A Very Big Sum 3 
+
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+fn aVeryBigSum(ar: &[i64]) -> i64 {
+    ar.iter().sum() 
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let _ar_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let ar: Vec<i64> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i64>().unwrap())
+        .collect();
+
+    let result = aVeryBigSum(&ar);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
+
+
+// Diagonal Difference 4 
+
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fn diagonalDifference(arr: &[Vec<i32>]) -> i32 {
+    let n = arr.len(); 
+    let mut primary_diagonal_sum = 0;
+    let mut secondary_diagonal_sum = 0;
+
+    for i in 0..n {
+        primary_diagonal_sum += arr[i][i]; 
+        secondary_diagonal_sum += arr[i][n - 1 - i]; 
+    }
+
+    (primary_diagonal_sum - secondary_diagonal_sum).abs() 
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let mut arr: Vec<Vec<i32>> = Vec::with_capacity(n as usize);
+
+    for i in 0..n as usize {
+        arr.push(Vec::with_capacity(n as usize));
+
+        arr[i] = stdin_iterator.next().unwrap().unwrap()
+            .trim_end()
+            .split(' ')
+            .map(|s| s.to_string().parse::<i32>().unwrap())
+            .collect();
+    }
+
+    let result = diagonalDifference(&arr);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
+
+// Plus Minus 5
+
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fn plusMinus(arr: &[i32]) {
+    let total = arr.len() as f32;
+
+    let positive_count = arr.iter().filter(|&&x| x > 0).count() as f32;
+    let negative_count = arr.iter().filter(|&&x| x < 0).count() as f32;
+    let zero_count = arr.iter().filter(|&&x| x == 0).count() as f32;
+
+    println!("{:.6}", positive_count / total);
+    println!("{:.6}", negative_count / total);
+    println!("{:.6}", zero_count / total);
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let arr: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    plusMinus(&arr);
+}
+
+
+// Staircase 6 
+
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+fn staircase(n: i32) {
+    for i in 1..=n {
+        let spaces = (n - i) as usize;
+        let hashes = i as usize;
+        println!("{:width$}{}", "", "#".repeat(hashes), width=spaces);
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    staircase(n);
+}
+
+
+// 
 
